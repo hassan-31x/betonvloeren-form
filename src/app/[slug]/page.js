8096f3faede8c1f14d1e808aee9aee5adf4314f2
hassan34319 <mh08062@st.habib.edu.pk>
2024-02-07 12:41:36 +0500

    Tried efficient caching for slug

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -2,12 +2,18 @@
 import { client } from '../utils/client';
 import VariationMain from "./VariationMain";
 
+export async function generateStaticParams() {
+  const variations = await client.fetch(`*[_type == "variations"]`);
+ 
+  return variations.map((variation) => ({
+    slug: variation.slug.current,
+  }))
+}
+
+export const revalidate = 60
 
 export default async function Index({ params }) {
   const slug = params.slug
-  if( !slug) {
-    return
-  }
   const homeData = await client.fetch('*[_type == "home"][0]');
   const uiData = await client.fetch('*[_type == "uiComponents"][0]');
   const variation = await client.fetch(`*[_type == "variations" && slug.current == "${slug}"][0]`);
